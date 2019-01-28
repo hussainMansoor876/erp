e66290d37bd08d8348bb0d2996dd3fd98591cccf
ahmedjalilqureshi <rapshek@gmail.com>
2019-01-28 16:15:26 +0500

    letters routes are defined

--- server/routes/modules/all/hrm.js
+++ server/routes/modules/all/hrm.js
@@ -6,126 +6,132 @@ const Employee = require('../../../models/employee');
 const Leaves = require('../../../models/leaves_applications');
 const Resignations = require('../../../models/resignation');
 const Notifications = require('../../../models/notifications');
-var auth = function(req,res,next){
+var auth = function (req, res, next) {
 
-       if(req.cookies.employee){
-          console.log("employee data",req.cookies.employee)
-          next();
-       }
-       else{
-          res.redirect('/');
-       }
-     
+    if (req.cookies.employee) {
+        console.log("employee data", req.cookies.employee)
+        next();
+    }
+    else {
+        res.redirect('/');
+    }
+
     // console.log(req.cookies);
-     
-  }
-  hrm.post('/add_applicant',(req,res)=>{
-      console.log("check");
-      console.log(req.body)
-  })
-  hrm.post('/',(req,res)=>{
+
+}
+hrm.post('/add_applicant', (req, res) => {
+    console.log("check");
+    console.log(req.body)
+})
+hrm.post('/', (req, res) => {
     let username = req.body.username;
     let password = req.body.password;
-    Employee.findOne({name:username},(err,user)=>{
-        if(user===null)
-        {
+    Employee.findOne({ name: username }, (err, user) => {
+        if (user === null) {
             res.end('employee does not exist')
         }
-        else{
-            if(user.password===password)
-            {
-                if(user.previledge.indexOf('hrm')!== -1 ||  true)
-                {   res.cookie('employee',JSON.stringify(user),{maxAge:1000*60*60*24});
+        else {
+            if (user.password === password) {
+                if (user.previledge.indexOf('hrm') !== -1 || true) {
+                    res.cookie('employee', JSON.stringify(user), { maxAge: 1000 * 60 * 60 * 24 });
                     res.redirect('/modules/hrm');
                 }
-                else{
+                else {
                     res.end('employee does not have previledge to access this module');
- 
+
                 }
             }
-            else{
+            else {
                 res.end("password does not match with user");
             }
         }
     })
-})  
-hrm.use(auth);  
-hrm.get('/',(req,res)=>{
+})
+hrm.use(auth);
+hrm.get('/', (req, res) => {
     res.render('modules/hrm/index');
 });
-
-hrm.get('/add_employee',(req,res)=>{
+hrm.get('/appointment_letter', (req, res) => {
+    res.render('modules/hrm/appointment_letter');
+});
+hrm.get('/contract_letter', (req, res) => {
+    res.render('modules/hrm/contract_letter');
+});
+hrm.get('/confirmation_letter', (req, res) => {
+    res.render('modules/hrm/confirmation_letter');
+})
+hrm.get('/add_employee', (req, res) => {
     res.render('modules/hrm/add_employee');
 });
-hrm.get('/add_labour',(req,res)=>{
+hrm.get('/add_labour', (req, res) => {
     res.render('modules/hrm/add_labour');
 });
-hrm.get('/add_applicant',(req,res)=>{
+hrm.get('/add_applicant', (req, res) => {
     res.render('modules/hrm/add_applicant');
 })
 
-hrm.get('/add_resignation',(req,res)=>{
+hrm.get('/add_resignation', (req, res) => {
     new Resignations({
-        employee_id:"32323232fggf",
-        employee_name:"Waqas khan",
-        dept:"Admin",
-        designation:"Manager",
-        reason:"move on to bigger org",
-        read:false,
-        status:0, // 0-> applied , 1->approved , -1->rejected
-        active:true
-    }).save(()=>{
+        employee_id: "32323232fggf",
+        employee_name: "Waqas khan",
+        dept: "Admin",
+        designation: "Manager",
+        reason: "move on to bigger org",
+        read: false,
+        status: 0, // 0-> applied , 1->approved , -1->rejected
+        active: true
+    }).save(() => {
         res.end('done');
     });
 })
 
-hrm.get('/get_resignations_info',(req,res)=>{
-    Resignations.find({},(err,resignations)=>{
-        res.json({resignations:resignations});
+hrm.get('/get_resignations_info', (req, res) => {
+    Resignations.find({}, (err, resignations) => {
+        res.json({ resignations: resignations });
         res.end();
     })
-   // res.render('modules/hrm/add_employee');
+    // res.render('modules/hrm/add_employee');
 });
 
-hrm.get('/employees_list',(req,res)=>{
+hrm.get('/employees_list', (req, res) => {
     res.render('modules/hrm/employees_list');
 });
 
-hrm.get('/resignations_info',(req,res)=>{
+hrm.get('/resignations_info', (req, res) => {
     res.render('modules/hrm/resignations_info');
 
 });
 
-hrm.get('/leaves_applications',(req,res)=>{
+hrm.get('/leaves_applications', (req, res) => {
     res.render('modules/hrm/leaves_applications');
 });
 
-hrm.post('/leaves_action',(req,res)=>{
+hrm.post('/leaves_action', (req, res) => {
     let id = req.body.id;
     let flag = req.body.flag;
-    Leaves.findByIdAndUpdate(id,{$set:{status:flag}},(err,leave)=>{
+    Leaves.findByIdAndUpdate(id, { $set: { status: flag } }, (err, leave) => {
         console.log(leave);
-        res.json({success:true});
+        res.json({ success: true });
         res.end();
     })
 })
 
-hrm.post('/resignations_action',(req,res)=>{
+hrm.post('/resignations_action', (req, res) => {
     let id = req.body.id;
     let flag = req.body.flag;
-    Resignations.findByIdAndUpdate(id,{$set:{status:flag}},(err,leave)=>{
+    Resignations.findByIdAndUpdate(id, { $set: { status: flag } }, (err, leave) => {
         console.log(leave);
-        res.json({success:true});
+        res.json({ success: true });
         res.end();
     })
 })
 
 
 
-hrm.post('/add_employee',(req,res)=>{
+hrm.post('/add_employee', (req, res) => {
     let form = new formidable.IncomingForm();
-      
-    form.parse(req,(err,fields,files)=>{
+
+    form.parse(req, (err, fields, files) => {
         let obj = {};
         obj.name = fields.name;
         obj.father_name = fields.father_name;
@@ -145,34 +151,36 @@ hrm.post('/add_employee',(req,res)=>{
         obj.basic_salary = fields.basic_salary;
         obj.dept = fields.dept;
         obj.designation = fields.designation;
-        new Notifications({title:'Orientation and training',
-                        detail:'New employee named '+obj.name+' joined in '+obj.dept+' department , please take his orientation',
-                        from:'HR' ,
-                        to:obj.dept}).save();
-       // console.log(obj);
-         let unique =  Math.floor((Math.random()*100000)+(Math.random()*100000));
-         var oldpath = files.picture.path;
-        var path_new =path.join("./public/assets/pictures/employees/picture_"+unique+"_"+files.picture.name);
-        var db_path = path.join("./employees/picture_"+unique+"_"+files.picture.name);
+        new Notifications({
+            title: 'Orientation and training',
+            detail: 'New employee named ' + obj.name + ' joined in ' + obj.dept + ' department , please take his orientation',
+            from: 'HR',
+            to: obj.dept
+        }).save();
+        // console.log(obj);
+        let unique = Math.floor((Math.random() * 100000) + (Math.random() * 100000));
+        var oldpath = files.picture.path;
+        var path_new = path.join("./public/assets/pictures/employees/picture_" + unique + "_" + files.picture.name);
+        var db_path = path.join("./employees/picture_" + unique + "_" + files.picture.name);
         obj.picture = db_path;
-        new Employee(obj).save(()=>{
-            fs.copyFile(oldpath,path_new,(err)=>{
-                if(err){
+        new Employee(obj).save(() => {
+            fs.copyFile(oldpath, path_new, (err) => {
+                if (err) {
                     throw err
                 }
-                fs.unlink(oldpath,(err_4)=>{
-                    if(err_4){
+                fs.unlink(oldpath, (err_4) => {
+                    if (err_4) {
                         throw err_4
                     }
                     console.log(obj);
                     res.redirect('./add_employee')
-                   
+
                 })
             })
-            
+
         });
     });
-   
+
 });
 
 module.exports = hrm;
